41f4fd600f8178f232afaf771a7d019e7ed478b0
t-harun <77989825+t-harun@users.noreply.github.com>
2021-03-31 17:33:23 -0400

    Added discord code to work with coinflip

--- coinflip.py
+++ coinflip.py
@@ -1,7 +1,19 @@
+import discord
+import os
 import random
+
+client = discord.Client(command_prefix="-")
 
 coin = random.randint(0, 1)
 if coin == 0:
     print( 'Heads' )
 else:
     print( 'Tails' )
+
+@client.command()
+async def coinflip(ctx):
+    result = ["HEADS", "TAILS"]
+    randomFlip = random.choice(result)
+    await ctx.channel.send(randomFlip)
+
+client.run('ODIzOTIyODMwOTI4Mzc5OTI0.YFn37g.qBiNOnlxbAgc7n4jfu9GQi2dkQk')
