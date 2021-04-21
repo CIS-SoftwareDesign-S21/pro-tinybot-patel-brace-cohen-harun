ae5c3d75424df6a3c9de957592a3365ef4bac79b
RaymondLaubert <59926120+RaymondLaubert@users.noreply.github.com>
2021-04-21 14:35:03 -0400

    New Leaderboard Progress

--- leaderb_impl.py
+++ leaderb_impl.py
@@ -4,53 +4,102 @@ from sqlite3 import Error
 
 class sqliteLeaderboard():
 
-    # Try to Establish Database Connection to the SQLite Database
-    try:
-        conn = sqlite3.connect("leaderboard.db")
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-
-    # Create a Cursor Object from the Connection
-    cursor = conn.cursor()
-
-    # Variables for Creating the Table for Users on the Leaderboard
-    sql_create_user_table = ''' CREATE TABLE IF NOT EXISTS users(
-        USER_ID INTEGER UNIQUE,
-        USER_NAME TEXT NOT NULL,
-        WINS INTEGER,
-        LOSSES INTEGER
-    )'''
-
-    # Create the Table if it doesn't Exist
-    cursor.execute(sql_create_user_table)
-
-    '''
-    # Create the Database Connection to the SQLite Database
+    # Function to Create the Database Connection to the SQLite Database
     def create_connection():
 
-        # Instantiate Connection as None
+        # Instantiate Connection Variable
         conn = None
 
-        # Try to Connect the Connection to the SQLite Database
+        # Try to Establish Database Connection to the SQLite Database
         try:
             conn = sqlite3.connect("leaderboard.db")
             print(sqlite3.version)
         except Error as e:
             print(e)
-        
+
         # Return the Connection
         return conn
 
+    
     # Function to Create a Table in the SQLite Database
-    def create_table(conn, create_table_sql):
+    def create_table(conn):
 
-        # Try to Create the Table using the Connection
+        # Create a Cursor Object from the Connection
+        cursor = conn.cursor()
+
+        # Variables for Creating the Table for Users on the Leaderboard
+        sql_create_user_table = ''' CREATE TABLE IF NOT EXISTS USERS(
+            USER_ID INTEGER UNIQUE,
+            USER_NAME TEXT NOT NULL,
+            WINS INTEGER,
+            LOSSES INTEGER
+        )'''
+
+        # Try to Create the Table (if it doesn't Exist) using the Cursor
         try:
-            c = conn.cursor()
-            c.execute(create_table_sql
+            cursor.execute(sql_create_user_table)
         except Error as e:
             print(e)
 
         return
-    '''
+
+    # Function to Insert a User into the Users Table
+    def insertUser(ctx, conn, userID, userName):
+
+        # Create a Cursor Object from the Connection
+        cursor = conn.cursor()
+
+        # Try to Insert the User into the User Table
+        try:
+            cursor.execute(''' INSERT INTO USERS(USER_ID, USER_NAME, WINS, LOSSES)
+                VALUES (f'{userID}', {userName}, 0, 0)
+            ''')
+        except Error as e:
+            print(e)
+
+        # Commit the Changes to the Database
+        conn.commit()
+
+        return
+
+
+    # Function to Update the Leaderboard Wins and Loses
+    def updateLeaderb(ctx, conn, winnerID, loserID, winnerName, loserName):
+
+        # Create a Cursor Object from the Connection
+        cursor = conn.cursor()
+
+        # Fetching all the Rows of Data before the Update
+        print("Contents of the User Table (Before): ")
+        cursor.execute('''SELECT * FROM USERS''')
+        print(cursor.fetchall())
+
+        # Make the Update
+        sql_update_winner = '''UPDATE USERS SET WINS=WINS+1 WHERE USER_ID={winnerID}'''
+        cursor.execute(sql_update_winner)
+        sql_update_loser = '''UPDATE USERS SET LOSSES=LOSSES+1 WHERE USER_ID={loserID}'''
+        cursor.execute(sql_update_loser)
+        print("Update Completed")
+
+        # Fetching all the Rows of Data after the Update
+        print("Contents of the User Table (After): ")
+        cursor.execute('''SELECT * FROM USERS''')
+        print(cursor.fetchall())
+
+        # Commit Changes Made to the Database
+        conn.commit()
+
+        return
+
+
+    # Function to close the Connection
+    def close_connection(ctx, conn):
+
+        # Try to Close the Connection
+        try:
+            conn.close()
+            print("Connection Successfully Closed.")
+        except Error as e:
+            print(e)
+
+        return
